1d3fc04a64fed033adfd50f91987d470a0599944
DamsFX <contact@hounddd.fr>
2021-06-27 04:26:31 +0200

    Add trait for validated signed data

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -10,26 +10,10 @@ use Omnipay\Common\Exception\InvalidResponseException;
 class CompletePurchaseRequest extends AbstractRequest
 {
 
-    public $liveEndpoint = 'https://paiement.systempay.fr/vads-payment/';
-
-
-    public function getData()
-    {
-        $signature = $this->generateSignature($this->httpRequest->request->all());
-        if (strtolower($this->httpRequest->request->get('signature')) !== $signature) {
-            throw new InvalidResponseException('Invalid signature');
-        }
-
-        return $this->httpRequest->request->all();
-    }
+    use GetValidatedSignedDataTrait;
 
     public function sendData($data)
     {
         return $this->response = new CompletePurchaseResponse($this, $data);
-    }
-
-    public function getEndpoint()
-    {
-        $this->liveEndpoint;
     }
 }
